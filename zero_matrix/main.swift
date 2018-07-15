afd9d311922aff75b9ff50081eee4acf3eb66b39
Andy <ajlewis1@me.com>
2018-07-15 14:44:07 -0600

    Initial Commit

---
+++ zero_matrix/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  zero_matrix
+//
+//  Created by Andrew Lewis on 7/15/18.
+//  Copyright © 2018 Andrew Lewis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
